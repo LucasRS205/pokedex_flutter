3e228f43d53a07d90ce170807e409eb587ef85c8
LucasRS205 <lucasrasil15@gmail.com>
2026-03-16 09:37:17 -0300

    Fix Bugs

--- lib/pokemon_list.dart
+++ lib/pokemon_list.dart
@@ -16,6 +16,9 @@ class _PokemonListState extends State<PokemonList> {
   List <Map<String, dynamic>> pokemons = [];
   List <Map<String, dynamic>> filterPokemons = [];
 
+  bool isLoading = true;
+  String errorMessage =  "";
+
   @override
   void initState() {
     super.initState();
@@ -23,27 +26,41 @@ class _PokemonListState extends State<PokemonList> {
   }
 
  Future fetchPokemons() async {
-  final response = await http.get(
-    Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=151"),
-  );
+  try {
+    setState(() {
+      isLoading = true;
+      errorMessage = "";
+    });
 
-  final data = jsonDecode(response.body);
-  final results = data["results"] as List;
+    final response = await http.get(
+      Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=151"),
+    ).timeout(const Duration(seconds: 15));
 
-  List<Map<String, dynamic>> loadedPokemons = [];
+    final data = jsonDecode(response.body);
+    final results = data["results"] as List;
 
-  for (int i = 0; i < results.length; i++) {
-    loadedPokemons.add({
-      "name": results[i]["name"],
-      "id": i + 1,
+    List<Map<String, dynamic>> loadedPokemons = [];
+
+    for (int i = 0; i < results.length; i++) {
+      loadedPokemons.add({
+        "name": results[i]["name"],
+        "id": i + 1,
+      });
+    }
+
+    setState(() {
+      pokemons = loadedPokemons;
+      filterPokemons = loadedPokemons;
+      isLoading = false;
+    });
+  } catch (e) {
+    setState(() {
+      isLoading = false;
+      errorMessage = "Erro ao carregar os pokemons: $e";
     });
   }
-
-  setState(() {
-    pokemons = loadedPokemons;
-    filterPokemons = loadedPokemons;
-  });
 }
+
   void searchPokemon(String query) {
     final input = query.toLowerCase();
 
@@ -176,91 +193,106 @@ class _PokemonListState extends State<PokemonList> {
 
 
        Expanded(
-  child: filterPokemons.isEmpty
+  child: isLoading
       ? const Center(child: CircularProgressIndicator())
-      : GridView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: filterPokemons.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 0.9,
-          ),
-          itemBuilder: (context, index) { // itemBuilder
-  final pokemon = filterPokemons[index];
-  final pokemonId = pokemon["id"];
-
-  return Container(
-    margin: const EdgeInsets.all(4),
-    decoration: BoxDecoration(
-      color: getSimpleCardColor(pokemonId),
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: const [
-        BoxShadow(
-          color: Colors.black26,
-          blurRadius: 6,
-          offset: Offset(2, 4),
-        ),
-      ],
-    ),
-    child: Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(16),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PokemonDetails(
-                name: pokemon["name"],
-                id: pokemonId,
-              ),
-            ),
-          );
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "#${pokemonId.toString().padLeft(3, '0')}",
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white70,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 6),
-              Text(
-                pokemon["name"].toUpperCase(),
+      : errorMessage.isNotEmpty
+          ? Center(
+              child: Text(
+                errorMessage,
+                style: const TextStyle(fontSize: 16),
                 textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
               ),
-              const SizedBox(height: 6),
-              Expanded(
-                child: Hero(
-                  tag: "pokemon-$pokemonId",
-                child: Image.network(
-                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokemonId.png",
-                  fit: BoxFit.contain,
-                  scale: 0.7
+            )
+          : filterPokemons.isEmpty
+              ? const Center(
+                  child: Text(
+                    "Nenhum Pokémon encontrado.",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                )
+              : GridView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: filterPokemons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.9,
+                  ),
+                  itemBuilder: (context, index) {
+                    final pokemon = filterPokemons[index];
+                    final pokemonId = pokemon["id"];
+
+                    return Container(
+                      margin: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: getSimpleCardColor(pokemonId),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            offset: Offset(2, 4),
+                          ),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(16),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PokemonDetails(
+                                  name: pokemon["name"],
+                                  id: pokemonId,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(12),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "#${pokemonId.toString().padLeft(3, '0')}",
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 6),
+                                Text(
+                                  pokemon["name"].toUpperCase(),
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 6),
+                                Expanded(
+                                  child: Hero(
+                                    tag: "pokemon-$pokemonId",
+                                    child: Image.network(
+                                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokemonId.png",
+                                      fit: BoxFit.contain,
+                                      scale: 0.7,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-},
-        ),
 ),
       ]),
     );
